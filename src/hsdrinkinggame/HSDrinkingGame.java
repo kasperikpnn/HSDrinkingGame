65d3843f8c6667212fc333aa994ef8d59f3be1f2
Sampo Vuorinen <sampo.vuorinen@edu.lahti.fi>
2018-04-29 21:00:54 +0300

    homo

--- src/hsdrinkinggame/HSDrinkingGame.java
+++ src/hsdrinkinggame/HSDrinkingGame.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hsdrinkinggame;
-
-/**
- *
- * @author Kasperi
- */
 public class HSDrinkingGame {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        boolean urmomgay = true;
+        while (urmomgay == true) {
+            System.out.println("ur mom gay");
+        }
     }
     
 }

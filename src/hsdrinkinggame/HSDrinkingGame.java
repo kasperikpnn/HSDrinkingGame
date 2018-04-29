65c77d5464e57a6b8f9cd54d317ed1dde86b38b4
Sampo Vuorinen <sampo.vuorinen@edu.lahti.fi>
2018-04-29 23:35:38 +0300

    homo

--- src/hsdrinkinggame/HSDrinkingGame.java
+++ src/hsdrinkinggame/HSDrinkingGame.java
@@ -1,6 +1,8 @@
 package hsdrinkinggame;
 public class HSDrinkingGame {
+
     public static void main(String[] args) {
+//eetu on nisti
         boolean urmomgay = true;
         while (urmomgay == true) {
             System.out.println("ur mom gay");

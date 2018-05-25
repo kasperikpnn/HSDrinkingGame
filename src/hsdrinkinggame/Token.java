000721a9caeb6f6dc2aa4f92b69936f7be6e68e2
Kasperi <Kasperi@192.168.0.100>
2018-05-25 22:03:37 +0300

    placeholder string -> image

--- src/hsdrinkinggame/Token.java
+++ src/hsdrinkinggame/Token.java
@@ -2,9 +2,13 @@ package hsdrinkinggame;
 import javafx.scene.image.Image;
 
 public class Token {
-    private String test; // väliaikanen kunnes jaksan piirtää paintilla tokenit
+    private Image image; // väliaikanen kunnes jaksan piirtää paintilla tokenit
     
-    public Token(String test) {
-        this.test = test;
+    public Token(Image image) {
+        this.image = image;
+    }
+    
+    public Image getImage() {
+        return this.image;
     }
 }
